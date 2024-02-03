f2017c23fb5f7b0b20b92e4147d8c455fa5ee761
ViraMesha <wira.mescha@gmail.com>
2024-02-03 20:10:22 +0200

    feat: Modify cart

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -13,6 +13,9 @@ export const Cart = ({ toggleMenu }: CartProps) => {
           fill="var(--secondary-text-color)"
           className="w-8 h-8 md:w-5 md:h-5 hover:scale-125 trans"
         />
+        <span className="bg-teal-900 text-white absolute top-5 right-3 w-5 h-5 rounded-full flex justify-center items-center text-[13px]">
+          10
+        </span>
       </Link>
     </li>
   );
