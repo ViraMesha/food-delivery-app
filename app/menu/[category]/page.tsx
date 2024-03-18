6621bf0dc4935aed30525de6d92c2d8403bb8563
ViraMesha <wira.mescha@gmail.com>
2024-03-18 22:02:49 +0200

    feat: Implement route for fetching products and integrate data retrieval in component and page

--- app/menu/[category]/page.tsx
+++ app/menu/[category]/page.tsx
@@ -1,6 +1,25 @@
 import PizzasList from "@/components/MenuCategory/PizzasList";
 
-const CategoryPage = () => {
+const getData = async (category: string) => {
+  const res = await fetch(
+    `http://localhost:3000/api/products?category=${category}`,
+    {
+      cache: "no-store",
+    }
+  );
+
+  if (!res.ok) throw new Error("Failed!");
+
+  return res.json();
+};
+
+type Props = {
+  params: { category: string };
+};
+
+const CategoryPage = async ({ params }: Props) => {
+  const products = await getData(params.category);
+
   return (
     <section className="py-10 md:py-20 xl:py-30">
       <div className="wrapper">
--- components/Home/Featured.tsx
+++ components/Home/Featured.tsx
@@ -2,7 +2,18 @@ import { featuredContent } from "@/lib/data";
 
 import { FeaturedItem } from ".";
 
-export const Featured = () => {
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/products", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) throw new Error("Failed!");
+
+  return res.json();
+};
+
+export const Featured = async () => {
+  // const featuredContent: TPizza[] = await getData();
   return (
     <section className="w-screen overflow-auto overscroll-x-contain text-[var(--secondary-text-color)]">
       {/* Products' list wrapper */}
