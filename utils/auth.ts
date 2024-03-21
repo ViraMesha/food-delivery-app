40c03ee4c06e7727b54786007eb44f9c6d0a23f9
ViraMesha <wira.mescha@gmail.com>
2024-03-21 21:57:36 +0200

    feat: Add prisma models for auth

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,7 +1,11 @@
 import { getServerSession, NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+import { prisma } from "./connect";
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
