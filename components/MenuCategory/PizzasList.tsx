0d0f0fea3a14778e9c52f6120a0fb71cdb3aa8be
ViraMesha <wira.mescha@gmail.com>
2024-03-29 23:49:56 +0200

    feat: Implement products data fetching

--- components/MenuCategory/PizzasList.tsx
+++ components/MenuCategory/PizzasList.tsx
@@ -2,10 +2,14 @@ import { pizzas } from "@/lib/data";
 
 import PizzaItem from "./PizzaItem";
 
-const PizzasList = () => {
+type Props = {
+  products: TPizza[];
+};
+
+const PizzasList = ({ products }: Props) => {
   return (
     <ul className="grid grid-cols-2 gap-[15px] md:grid-cols-3 xl:grid-cols-4 xl:gap-[30px]">
-      {pizzas.map((pizza) => (
+      {products.map((pizza) => (
         <PizzaItem key={pizza.id} pizza={pizza} />
       ))}
     </ul>
